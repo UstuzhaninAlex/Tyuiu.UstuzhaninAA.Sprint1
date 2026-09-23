cdd7e09cef66fde8ae3585ef61395472da6d7640
Алексей Устюжанин <aa.ustuzhanin@gmail.com>
2026-09-23 21:42:38 +0500

    Task0

---
+++ Tyuiu.UstuzhaninAA.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tyuiu.UstuzhaninAA.Sprint1.Task0.V0.Lib;
+namespace Tyuiu.UstuzhaninAA.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Устюжанин А. А. | ИСПб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнила: Устюжанин Алексей Андреевич | ИСПб-26-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет 2 * 9 + 7 * 2                     *");
+            Console.WriteLine("* и печатает результат на экран.                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 2 * 9 + 7 * 2                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tyuiu.UsztuzhaninAA.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.UstuzhaninAA.Sprint1.Task0.V0.Lib;
+namespace Tyuiu.UsztuzhaninAA.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(32, res);
+
+        }
+    }
+}
