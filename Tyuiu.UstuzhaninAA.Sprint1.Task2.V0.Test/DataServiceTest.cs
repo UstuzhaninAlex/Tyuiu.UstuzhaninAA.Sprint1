681823e06cd18173a1e48d3aaaf33d3ebae6e5f9
Алексей Устюжанин <aa.ustuzhanin@gmail.com>
2026-09-24 14:46:02 +0500

    Task2

---
+++ Tyuiu.UstuzhaninAA.Sprint1.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.UstuzhaninAA.Sprint1.Task2.V0.Lib;
+namespace Tyuiu.UstuzhaninAA.Sprint1.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 158;
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(4.013, res);
+        }
+    }
+}
---
+++ Tyuiu.UstuzhaninAA.Sprint1.Task2.V0/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.UstuzhaninAA.Sprint1.Task2.V0.Lib;
+namespace Tyuiu.UstuzhaninAA.Sprint1.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Устюжанин А. А. | ИСПб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнила: Устюжанин Алексей Андреевич | ИСПб-26-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите количество дюймов: ");
+            int inches = Convert.ToInt32(Console.ReadLine());
+            double meters = ds.ConvertInchToKm(inches);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Растояние в метрах = {meters}");
+            Console.ReadKey();
+        }
+    }
+}
